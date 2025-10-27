fc8536365539367a0ab12f1ca75c854ed06018d6
grosen <rosen.9@wright.edu>
2025-10-26 22:22:36 -0400

    All methods migrated from Project Prompt. Some started.

--- HashTable.h
+++ HashTable.h
@@ -1,3 +1,72 @@
-/**
- * HashTable.h
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <optional>
+#include <string>
+#include <vector>
+
+/*
+ * Greg Rosen
+ * Project 4: HashTable
+ * Declaration for HashTable class
  */
+
+/*
+ * HashTable: A map with string keys and non-negative integer values
+ *
+ */
+class HashTable {
+private:
+    class HashTableBucket {
+    public:
+        /*
+         * BucketType: History type of HashTableBucket
+         * NORMAL - Bucket contains key-value pair
+         * ESS - "Empty Since Start" - Bucket has never been filled since HashTable was last created/resized
+         * EAR - "Empty After Removal" - Tombstone - Bucket is empty, but has been filled since HashTable was last created/resized
+         */
+        enum class BucketType {NORMAL, ESS, EAR};
+
+    private:
+        std::string key; // Key for mapping
+        size_t value; // Value mapped
+        BucketType type; // See enum BucketType
+
+    public:
+        HashTableBucket(); // Default constructor
+        HashTableBucket(const std::string& key, const size_t& value); // Parameterized constructor
+
+        [[nodiscard]] std::string getKey() const;
+        [[nodiscard]] size_t getValue() const;
+        [[nodiscard]] BucketType getType() const;
+
+        [[nodiscard]] bool isEmpty() const; // Predicate for determining if bucket is empty
+
+        void load(const std::string& key, const size_t& value); // Fills bucket with key-value pair
+    };
+
+    std::vector<HashTableBucket> tableData; // The Hash Table itself, implemented as a vector of HashTableBucket elements
+    std::vector<size_t> offsets; // Offsets for pseudo-random probing
+    size_t numFilled; // The number of filled buckets in the Hash Table (NORMAL status);
+
+public:
+    explicit HashTable(size_t initCapacity = 8); // Default and parameterized constructor
+
+    size_t& operator[](const std::string& key); // Subscript operator overload
+
+    [[nodiscard]] double alpha() const; // Getter for alpha (load factor)
+    [[nodiscard]] size_t capacity() const; // Getter for capacity (# of buckets in table)
+    [[nodiscard]] size_t size() const; // Getter for size (# of filled buckets in table)
+    [[nodiscard]] std::vector<std::string> keys() const; // Getter for list of keys currently used in table
+    [[nodiscard]] std::optional<size_t> get(const std::string& key) const; // Getter for value stored using given key
+
+    [[nodiscard]] bool contains(const std::string& key) const; // Predicate for determining whether given key is used in table
+
+    bool insert(const std::string& key, const size_t& value); // Insert key-value pair into table, resizing if required
+    bool remove(const std::string& key); // Remove key-value pair from table
+
+    friend std::ostream& operator<<(std::ostream& os, const HashTableBucket& bucket); // Stream insertion operator overload HashTableBucket
+    friend std::ostream& operator<<(std::ostream& os, const HashTable& hashTable); // Stream insertion operator overload for HashTable
+};
+
+#endif // HASHTABLE_H
